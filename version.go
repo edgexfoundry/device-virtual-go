0f54f9fd90af1d87850aa84817de94ca5fc3abc8
Bruce <weichou1229@gmail.com>
2019-05-20 19:10:50 +0800

    Load version number from VERSION file The Version field will override when running the build command(make build)
    Signed-off-by: Bruce <weichou1229@gmail.com>

--- version.go
+++ version.go
@@ -1,10 +1,10 @@
 // -*- Mode: Go; indent-tabs-mode: t -*-
 //
-// Copyright (C) 2018 IOTech Ltd
+// Copyright (C) 2018-2019 IOTech Ltd
 //
 // SPDX-License-Identifier: Apache-2.0
 
 package device_virtual
 
 // Global version for device-sdk-go
-const Version string = "1.0.0"
+var Version string = "to be replaced by makefile"
